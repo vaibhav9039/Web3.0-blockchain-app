1141328f5fde7811e796fd123ebc084c189bf079
Vaibhav Kushwaha <vaibhavkushwahaq345@gmail.com>
2023-05-24 11:51:03 +0530

    Crowdfunding app link added

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,36 +1,50 @@
-import React from 'react'
+import React from "react";
 import { HiMenuAlt4 } from "react-icons/hi";
 import { AiOutlineClose } from "react-icons/ai";
+import logo from "../../images/etherswap.png";
 
-import logo from "../../images/logo.png";
-
-const NavBarItem = ({ title, classprops }) => (
-  <li className={`mx-4 cursor-pointer ${classprops}`}>{title}</li>
+const NavBarItem = ({ title, classprops, linkTo }) => (
+  <li className={`mx-4 cursor-pointer ${classprops}`}>
+    <a href={linkTo}>{title}</a>
+  </li>
 );
+
 const Navbar = () => {
   const [toggleMenu, setToggleMenu] = React.useState(false);
+
   return (
     <nav className="w-full flex md:justify-center justify-between items-center p-4">
       <div className="md:flex-[0.5] flex-initial justify-center items-center">
         <img src={logo} alt="logo" className="w-32 cursor-pointer" />
       </div>
       <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
-        {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index) => (
-          <NavBarItem key={item + index} title={item} />
+        {[
+          {
+            title: "Crowdfunding",
+            linkTo: "https://crowdfunding-crypto.netlify.app/",
+          },
+          { title: "Exchange" },
+          { title: "Tutorials" },
+          { title: "Wallets" },
+        ].map((item, index) => (
+          <NavBarItem
+            key={item.title + index}
+            title={item.title}
+            linkTo={item.linkTo}
+          />
         ))}
         <li className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">
           Login
         </li>
       </ul>
       <div className="flex relative">
-        {!toggleMenu && (
+        {!toggleMenu ? (
           <HiMenuAlt4
             fontSize={28}
             className="text-white md:hidden cursor-pointer"
             onClick={() => setToggleMenu(true)}
           />
-        )}
-        {toggleMenu && (
+        ) : (
           <AiOutlineClose
             fontSize={28}
             className="text-white md:hidden cursor-pointer"
@@ -45,20 +59,27 @@ const Navbar = () => {
             <li className="text-xl w-full my-2">
               <AiOutlineClose onClick={() => setToggleMenu(false)} />
             </li>
-            {["Market", "Exchange", "Tutorials", "Wallets"].map(
-              (item, index) => (
-                <NavBarItem
-                  key={item + index}
-                  title={item}
-                  classprops="my-2 text-lg"
-                />
-              )
-            )}
+            {[
+              {
+                title: "Crowdfunding",
+                linkTo: "https://crowdfunding-crypto.netlify.app/",
+              },
+              { title: "Exchange" },
+              { title: "Tutorials" },
+              { title: "Wallets" },
+            ].map((item, index) => (
+              <NavBarItem
+                key={item.title + index}
+                title={item.title}
+                linkTo={item.linkTo}
+                classprops="my-2 text-lg"
+              />
+            ))}
           </ul>
         )}
       </div>
     </nav>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
